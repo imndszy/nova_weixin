a00118db00f699868aaa56e471896f153ac58414
szy <shizhenyu96@gmail.com>
2016-07-13 12:23:59 +0800

    test network

--- app/weixin/weixinconfig.py
+++ app/weixin/weixinconfig.py
@@ -5,8 +5,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-APP_ID = 'wx92a9c338a5d02f38'
-SECRET = 'aed361bef86c682fcc4f49db2df58588'
+APP_ID = 'wx92a9c338'
+SECRET = 'aed361bef4f49db2df58588'
 TOKEN = ''
 TEMPLATE_ID = ''
 MENU = {
