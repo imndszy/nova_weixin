52cc2bc515ca52017574c042aa42f92bd30e25cb
szy <shizhenyu96@gmail.com>
2016-10-12 10:01:37 +0800

    fix import bug

--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@
 # github: https://github.com/imndszy
 import os
 import sys
-import nova_weixin.app
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import nova_weixin.app
+
 
 app = nova_weixin.app.create_app(os.getenv('FLASK_CONFIG') or 'production')
 
