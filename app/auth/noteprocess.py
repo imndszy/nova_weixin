e81675a51f47b0c7c1216752d8f09b45c69dc3fe
szy <shizhenyu96@gmail.com>
2017-01-17 20:23:49 +0800

    optimize db functions in noteprocess

--- app/auth/noteprocess.py
+++ app/auth/noteprocess.py
@@ -3,6 +3,7 @@
 # github: https://github.com/imndszy
 import logging
 
+from nova_weixin.packages.novamysql import insert
 from nova_weixin.app.lib.database import mysql
 from nova_weixin.app.weixin.template import send_common_template_msg
 from nova_weixin.app.nova.get_user_info import get_openid
@@ -14,38 +15,50 @@ def note_index(stu_list, nid):
     stu_text = ''
     for i in stu_list:
         stu_text = stu_text + i
-    sql = "insert into noteindex "\
-          "values(%s,%s,0,'','%s',0)" % (nid, nid, stu_text)
-
-    @mysql(sql)
-    def insert(results=''):
-        return results
-
-    insert()
-    return 0
+    result = insert('noteindex', nid=nid, publishTime=nid, sort=0, topic='', stuids=stu_text, expire=0)
+    # sql = "insert into noteindex "\
+    #       "values(%s,%s,0,'','%s',0)" % (nid, nid, stu_text)
+    #
+    # @mysql(sql)
+    # def insert(results=''):
+    #     return results
+    #
+    # insert()
+    if result == 1:
+        return 0
+    else:
+        return -1
 
 
 def note_content(arti_url, image_url, title, nid):
     temp_tuple = (nid, title, image_url, arti_url)
-    sql = "insert into notecontent "\
-          "values(%s,'%s','cac','','%s','%s',0)" % temp_tuple
-
-    @mysql(sql)
-    def insert(results=''):
-        return results
-    insert()
-    return 0
+    result = insert('notecontent', nid=nid, title=title, publisher='cac', detail='', picurl=image_url, url=arti_url, tid=0)
+    # sql = "insert into notecontent "\
+    #       "values(%s,'%s','cac','','%s','%s',0)" % temp_tuple
+    #
+    # @mysql(sql)
+    # def insert(results=''):
+    #     return results
+    # insert()
+    if result == 1:
+        return 0
+    else:
+        return -1
 
 
 def note_response(nid):
-    sql = "insert into noteresponse values(%s,0,0,'','',0)" % nid
-
-    @mysql(sql)
-    def insert(results=''):
-        return results
-
-    insert()
-    return 0
+    result = insert('noteresponse',nid=nid, earlistread=0, latestread=0, readlist='', readtime='', readpop=0)
+    # sql = "insert into noteresponse values(%s,0,0,'','',0)" % nid
+    #
+    # @mysql(sql)
+    # def insert(results=''):
+    #     return results
+    #
+    # insert()
+    if result == 1:
+        return 0
+    else:
+        return -1
 
 
 def send(_title, arti_url, stu_list):
