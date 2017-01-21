53fcf791f3813eaec158c79a75bb8f5b647df84c
szy <shizhenyu96@gmail.com>
2017-01-21 21:13:05 +0800

    support py2 & py3

--- menu.py
+++ menu.py
@@ -2,26 +2,28 @@
 # Author: shizhenyu96@gamil.com
 # github: https://github.com/imndszy
 import json
-import requests
 import sys
 
-if sys.version_info < (3, 0):
+from nova_weixin.app.config import PY2
+
+if PY2:
     reload(sys)
     sys.setdefaultencoding('utf8')
 
 from nova_weixin.app.weixin.get_acc_token import get_token
 from nova_weixin.app.weixin.weixinconfig import MENU
+from nova_weixin.packages.nova_wxsdk import WxApiUrl, CommunicateWithApi
 
 
 def create_menu():
     acc_token = get_token()
     if acc_token:
-        url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=%s" % acc_token
+        url = WxApiUrl.create_menu.format(access_token=acc_token)
         data = MENU
-        r = requests.post(url, data=json.dumps(data, ensure_ascii=False))
-        return r.text
+        return CommunicateWithApi.post_data(url, data=json.dumps(data, ensure_ascii=False).encode('utf8'))
     else:
-        return "failed to get access_token!--menu.py"
+        # log
+        return -1
 
 
 if __name__ == "__main__":
